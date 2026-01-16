44b8dfe44dab0437b1d39f7cb2436353cb0e4fd1
MOEP-Robotik <Moepdbg@outlook.de>
2026-01-16 16:05:47 +0100

    auth fortsetzung

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -6,10 +6,13 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Core\Auth;
 
 class AuthController{
-    public function login_request(string $user, string $password) {
-        $passfromDB = ;
+    public function login_request(string $user, string $password) { #returnt entweder false oder den jwt_token
+        $passfromDB = ; #von Datenbank nehmen
+        $user_id = ; #von Datenbank nehmen
         if (password_verify($password, $passfromDB)){
-            return true;
+            $auth = new Auth();
+            $jwt_token = $auth->generate_jwt($user_id)
+            return $jwt_token;
         } else {
             return false;
         }
